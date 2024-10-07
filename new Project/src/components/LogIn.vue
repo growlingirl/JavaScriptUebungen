9bca851e6e2800c8ed57179ebc1ea2ce3111661f
Jenny Mathys <jenny.mathys@kauz.ch>
2024-10-07 12:45:13 +0200

    Update LogIn.vue

--- new Project/src/components/LogIn.vue
+++ new Project/src/components/LogIn.vue
@@ -9,20 +9,25 @@ const titleId1 = ref('richtig')
 const titleId2 = ref('falsch')
 
 const Dif = computed (() => {
-    3 - count.value
+    return 4 - count.value
 })
 
-const Versuch = computed(() => {
-    return Dif.value > 0 ? ('noch' + Dif + 'Versuche übrig.') : 'keine Versuche mehr.'; 
-})
+//const Try = computed (() => {
+ //   return Dif - 1
+//})
 
 function increment() {
     count.value++;
 
     if (pw.value == pw0.value && Dif > 0) {
-        right.value = !right.value;
-    } 
+       right.value = !right.value; } 
 }
+
+const Versuch = computed(() => {
+    return Dif.value > 1 ? ('noch ' + Dif.value + ' Versuche übrig.') : 'keine Versuche mehr.'; 
+})
+
+
 
 </script>
 
@@ -30,16 +35,15 @@ function increment() {
     <div>
 <h1>Passworteingabe</h1>
 
-<form>
+<form @submit.prevent>
         <label for="pw">bitte gib das korrekte Passwort ein</label> <br>
 
         <input type="password" id="pw" v-model="pw"> <br> <br>
+    
+    <button @:click = 'increment' > Du hast {{ Versuch }} </button>
+</form>
 
-        <button @:click = 'increment' > Du hast {{ Versuch }} </button>
-
-    </form>
-
-    <h2 v-if='right.value' :id="titleId1">BERECHTIGT</h2>
+    <h2 v-if='right' :id="titleId1">BERECHTIGT</h2>
     <h2 v-else :id="titleId2">Du bist nicht berechtigt.</h2>
 
 </div>
